f167c4548ac2336ff0b9af80fba6e047cb287416
Edward Miller <miller.edwardg@gmail.com>
2021-02-28 14:31:44 -0600

    Initiate Project
    Initiate Project

---
+++ mappED/AutoMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BevDevIO.mappED
+{
+    public static class AutoMapper
+    {
+        public static T MapTo<S, T>(S source)
+        {
+            var target = (T)Activator.CreateInstance(typeof(T));
+            var sType = source.GetType();
+            return default(T);
+        }
+    }
+}
